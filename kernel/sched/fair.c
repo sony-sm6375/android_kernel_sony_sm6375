986658e2fc3f020ef03da206a56b7b080777ed91
jmpfbmx <jmpf.bmx@gmail.com>
2025-03-26 18:47:42 +0100

    kernel: sched: fair: Avoid unnecessary task detachment on newly idle CPUs
    In detach_tasks(), add extra checks to prevent detaching tasks when the destination
    runqueue (dst_rq) belongs to a CPU that has just become idle (CPU_NEWLY_IDLE) but
    already received tasks. This minimizes unnecessary load migrations that could
    increase latency. In particular:
    • If a newly idle CPU has tasks (nr_running > 0), exit early to avoid detaching more tasks.
    • Apply additional detachment conditions only when the CPU is not in the newly idle state.
    
    These changes help reduce latency and improve overall load balancing efficiency.
    
    Change-Id: I47035b2760abc33def02950f74835509f4b9f361
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8487,6 +8487,14 @@ redo:
 		if (env->idle != CPU_NOT_IDLE && env->src_rq->nr_running <= 1)
 			break;
 
+		/*
+		 * Another CPU can place tasks, since we do not hold dst_rq lock
+		 * while doing balancing. If newly idle CPU already got something,
+		 * give up to reduce a latency.
+		 */
+		if (env->idle == CPU_NEWLY_IDLE && env->dst_rq->nr_running > 0)
+			break;
+
 		p = list_last_entry(tasks, struct task_struct, se.group_node);
 
 		env->loop++;
@@ -8517,6 +8525,8 @@ redo:
 		if (sched_feat(LB_MIN) && load < 16 && !env->sd->nr_balance_failed)
 			goto next;
 
+
+		if (env->idle != CPU_NEWLY_IDLE) {
 		/*
 		 * p is not running task when we goes until here, so if p is one
 		 * of the 2 task in src cpu rq and not the running one,
@@ -8533,6 +8543,7 @@ redo:
 			(env->flags & LBF_IGNORE_BIG_TASKS)) &&
 			((load / 2) > env->imbalance))
 			goto next;
+		}
 
 		detach_task(p, env);
 		list_add(&p->se.group_node, &env->tasks);
